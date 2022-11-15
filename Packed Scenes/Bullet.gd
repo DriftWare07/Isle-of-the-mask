d60821606af125da0ee2326980167c90952ff47d
DriftWare07 <99690970+DriftWare07@users.noreply.github.com>
2022-11-15 14:38:25 -0600

    Pea shooter finished
    He now shoots his peas at you.
    
    shut up.

---
+++ Packed Scenes/Bullet.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocity = Vector2(200,0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("pew")
+
+
+func _physics_process(delta):
+	velocity = move_and_slide(velocity)
+
+
+
+func _on_Area2D_body_entered(body):
+	queue_free()
--- Packed Scenes/Plant Shooter.gd
+++ Packed Scenes/Plant Shooter.gd
@@ -4,13 +4,30 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var b = preload("res://Packed Scenes/Bullet.tscn")
+var bullet = b.instance()
+export var flipped = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimatedSprite.flip_h = flipped
 
-
+func _physics_process(delta):
+	if $ShootTimer.time_left < 1:
+		bullet = b.instance()
+		$AnimatedSprite.play("attack")
+		
+		bullet.set_global_position($Position2D.global_position)
+		if flipped:
+			bullet.velocity.x = 200
+		else:
+			bullet.velocity.x = -200
+		$ShootTimer.start()
+		yield($AnimatedSprite, "animation_finished")
+		var bs = get_parent().add_child(bullet)
+		
+		
+		$AnimatedSprite.play("idle")
 
 
 func _on_Plant_Shooter_area_entered(area):
